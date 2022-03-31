4c0f0d8d14e25acbabc2b0427da3a4339b22124d
Kundan Karn <kundankarn007@gmail.com>
2022-04-01 01:31:53 +0530

    docker updates

--- src/Data/api.js
+++ src/Data/api.js
@@ -2,10 +2,11 @@ import axios from "axios";
 
 // Note the "/" at the end of the URL. Dont remove it yourself.
 
-var Base_URL = process.env.Base_URL.replace(/\/$/, "");
+var Base_URL = process.env.REACT_APP_BaseURL;
+Base_URL = Base_URL.replace(/\/$/, "");
 
 //Use the Final_URL when you need to set URL for axios GET request
-const Final_URL= "https://"+Base_URL+":3000/findAllCourses";
+const Final_URL= Base_URL+":3000/findAllCourses";
 
 
 export default {
