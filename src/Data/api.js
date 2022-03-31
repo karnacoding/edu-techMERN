c678ad0c8f9afbc6b53379227c99fc5b4c9867f9
Kundan Karn <kundankarn007@gmail.com>
2022-04-01 01:03:24 +0530

    docker updates

--- src/Data/api.js
+++ src/Data/api.js
@@ -1,12 +1,8 @@
 import axios from "axios";
 
-/*Set Base_URL to the URL of the browser in the right pane ==>
- Note: Sample value has been set for your reference only. 
- Replace "1dod9q9ermmmm.educative.run/" with the copied URL */
-
 // Note the "/" at the end of the URL. Dont remove it yourself.
-var Base_URL = "16kmqv8wxemor.educative.run/";
-Base_URL = Base_URL.replace(/\/$/, "");
+
+var Base_URL = process.env.Base_URL.replace(/\/$/, "");
 
 //Use the Final_URL when you need to set URL for axios GET request
 const Final_URL= "https://"+Base_URL+":3000/findAllCourses";
